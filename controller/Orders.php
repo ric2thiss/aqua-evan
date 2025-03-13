08fa01dfd81d3f90b04789a728583eb747ef01dd
Ric Charles Paquibot <ric2thiss@gmail.com>
2025-03-13 11:32:47 +0800

    feature: added orders class

---
+++ controller/Orders.php
@@ -0,0 +1,27 @@
+
+<?php
+
+class Orders
+{
+    private PDO $dbconn;
+
+    public function __construct(PDO $dbconnection)
+    {
+        $this->dbconn = $dbconnection;
+    }
+
+    public function get_order_type()
+    {
+        // Insert
+        // Getter
+    }
+    
+    public function insert_activity()
+    {
+        // Insert
+    }
+
+
+
+
+}
